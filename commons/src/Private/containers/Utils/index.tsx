a885d555bec76df2f197c88ca1fbd458793613aa
Arix <arixgg@gmail.com>
2022-05-22 02:32:32 +0800

    finish utils page

--- commons/src/Private/containers/Utils/index.tsx
+++ commons/src/Private/containers/Utils/index.tsx
@@ -1,7 +1,32 @@
+import { Descriptions, Space, Tag } from "antd";
 import React from "react";
+import { Container } from "../../constants/styles";
 
 const UtilsPage: React.FC = () => {
-  return <div>Utils Page</div>;
+  return (
+    <Container>
+      <Descriptions title="DataUtils" column={1} bordered>
+        <Descriptions.Item
+          label={
+            <Space>
+              toSnakeCase <Tag color="geekblue">{`Function`}</Tag>
+            </Space>
+          }
+        >
+          Transforms object keys to snake_Case
+        </Descriptions.Item>
+        <Descriptions.Item
+          label={
+            <Space>
+              toCamelCase <Tag color="geekblue">{`Function`}</Tag>
+            </Space>
+          }
+        >
+          Transforms object keys to camelCase
+        </Descriptions.Item>
+      </Descriptions>
+    </Container>
+  );
 };
 
 export default UtilsPage;
