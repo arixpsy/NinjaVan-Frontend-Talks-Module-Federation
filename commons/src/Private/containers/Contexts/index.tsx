0c27590b91d449c15e686589e60660fd14b22a9d
Arix <arixgg@gmail.com>
2022-05-22 02:08:23 +0800

    finish context page

--- commons/src/Private/containers/Contexts/index.tsx
+++ commons/src/Private/containers/Contexts/index.tsx
@@ -1,8 +1,56 @@
+import { Descriptions, Space, Tag } from "antd";
 import React from "react";
 import { Container } from "../../constants/styles";
 
 const ContextsPage: React.FC = () => {
-  return <Container>Contexts Page</Container>;
+  return (
+    <Container>
+      <Descriptions title="Auth Context and Provider" column={1} bordered>
+        <Descriptions.Item label="Context Shape">
+          <div>
+            <pre>
+              {`
+{
+  isAuthenticated: boolean
+  scopes: {
+    [key: string]: boolean
+  };
+  handleLogout: Function
+}
+`}
+            </pre>
+          </div>
+        </Descriptions.Item>
+        <Descriptions.Item
+          label={
+            <Space>
+              isAuthenticated<Tag color='geekblue'>boolean</Tag>
+            </Space>
+          }
+        >
+          if user is currently logged in
+        </Descriptions.Item>
+        <Descriptions.Item
+          label={
+            <Space>
+              scopes <Tag color='geekblue'>{`Record<String, boolean>`}</Tag>
+            </Space>
+          }
+        >
+          Object with user scopes from AAA
+        </Descriptions.Item>
+        <Descriptions.Item
+          label={
+            <Space>
+              handleLogout<Tag color='geekblue'>Function</Tag>
+            </Space>
+          }
+        >
+          handles removing user auth cookie
+        </Descriptions.Item>
+      </Descriptions>
+    </Container>
+  );
 };
 
 export default ContextsPage;
